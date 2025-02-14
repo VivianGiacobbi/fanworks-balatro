a0f813e0c4ddf141449b135101b537e6be3e0208
BarrierTrio <payagoat@gmail.com>
2025-02-13 20:17:54 -0700

    3 jokers

---
+++ jokers/gq_lambiekins.lua
@@ -0,0 +1,43 @@
+local jokerInfo = {
+    name = 'Ms. Lambiekins',
+    config = {
+        extra = {
+			money = 1
+		}
+    },
+    rarity = 1,
+    cost = 5,
+    blueprint_compat = true,
+    eternal_compat = true,
+    perishable_compat = true,
+    hasSoul = true,
+    fanwork = 'gotequest',
+}
+
+function jokerInfo.loc_vars(self, info_queue, card)
+    info_queue[#info_queue+1] = {key = "guestartist0", set = "Other"}
+    return { vars = { card.ability.extra.money } }
+end
+
+function jokerInfo.in_pool(self, args)
+    for _, v in ipairs(G.playing_cards) do
+        if v.ability.effect == "Lucky Card" then
+            return true
+        end
+    end
+end
+
+function jokerInfo.calculate(self, card, context)
+    if context.individual and context.cardarea == G.play and not card.debuff then
+        if context.other_card.ability.effect == "Lucky Card" then
+            ease_dollars(to_big(card.ability.extra.money))
+			return {
+				message = localize('$')..card.ability.extra.money,
+				colour = G.C.MONEY,
+				card = card
+			}
+        end
+    end
+end
+
+return jokerInfo
