2577aa584767924d04f9f389ae192df007f2a0f5
NotDaedalus <0164907@gmail.com>
2025-03-21 13:40:12 +1100

    Firewalker working as intended, no art yet.

--- items/jokers/double_firewalker.lua
+++ items/jokers/double_firewalker.lua
@@ -16,11 +16,13 @@ function jokerInfo.loc_vars(self, info_queue, card)
     info_queue[#info_queue+1] = {key = "incomplete", set = "Other"}
 end
 
+local options = { 'j_popcorn', 'j_ice_cream'}
+
 function jokerInfo.calculate(self, card, context)
     if context.cardarea == G.jokers and context.final_scoring_step then
         if hand_chips*mult > G.GAME.blind.chips then
             G.E_MANAGER:add_event(Event({
-                SMODS.add_card({ key = 'j_popcorn', area = G.jokers, edition = 'e_negative' }),
+                SMODS.add_card({key = pseudorandom_element(options), area = G.jokers, edition = 'e_negative'}),
         }))
             end
         end
